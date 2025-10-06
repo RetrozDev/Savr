1016ce46331674f757db9a3781168b687bb7e203
RetrozDev <ryanbarbozapro@gmail.com>
2025-10-06 17:39:14 +0200

    feat(api): added cors

--- server/src/main.py
+++ server/src/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes import recipes, categories
+from dotenv import load_dotenv
+import os
+
+# Charger les variables d'environnement
+load_dotenv()
+
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "").split(",")  # supporte plusieurs URLs séparées par une virgule
+PORT = int(os.getenv("PORT", 8000))
 
 app = FastAPI()
-url = 'http//localhost:8000'
+
+# Middleware CORS pour autoriser seulement certaines origines
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI!"}
 
-# include recipes in router
+# Inclure les routers
 app.include_router(recipes.router)
 app.include_router(categories.router)
